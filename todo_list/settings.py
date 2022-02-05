ee8fa4c9a72a019646ea48d82a00aac7ff77d467
Egor Uvarov <52317049+josephRTi@users.noreply.github.com>
2022-02-06 00:49:51 +0300

    added CORS for developing new frontend app

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -63,7 +63,7 @@ MIDDLEWARE = [
 CORS_ALLOWED_ORIGINS = [
     'https://to-do-webapplication.herokuapp.com',
     'https://127.0.0.1',
-    'http://localhost:8080/#/'
+    'http://localhost:8080'
     '*',
 ]
 
